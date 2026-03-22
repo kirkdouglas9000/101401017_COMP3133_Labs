58b6df080d70fa1c722e99aee09ad6c5f2d452d6
kirkdouglas9000 <kirkdouglas400@gmail.com>
2026-03-22 01:09:24 -0400

    lab07

--- Lab07_101401017/exercise-2/my-app/src/app/app.ts
+++ Lab07_101401017/exercise-2/my-app/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-app');
+  protected readonly title = signal('Lab 07 Angular App');
 }
